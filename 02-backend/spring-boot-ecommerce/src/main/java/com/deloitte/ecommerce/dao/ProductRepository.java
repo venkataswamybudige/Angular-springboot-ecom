c1d1556dd5ebdd22ccc33994037c1b38d532adfd
venkataswamybudige <82580597+venkataswamybudige@users.noreply.github.com>
2023-03-31 22:15:46 +0530

    Added search by key rest  api

--- 02-backend/spring-boot-ecommerce/src/main/java/com/deloitte/ecommerce/dao/ProductRepository.java
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/deloitte/ecommerce/dao/ProductRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.deloitte.ecommerce.entity.Product;
 
@@ -12,4 +13,5 @@ import com.deloitte.ecommerce.entity.Product;
 public interface ProductRepository  extends JpaRepository<Product, Long>{
 	
 	Page<Product> findByCategoryId(@Param("id") Long id,Pageable pageable);
+	Page<Product> findByNameContaining(@RequestParam("name") String name,Pageable pageable);
 }
